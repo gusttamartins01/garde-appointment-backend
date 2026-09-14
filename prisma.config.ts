331dc8b9fca2942e692ae2c459a6a07893f211b1
gustavo martins <luizgustams@gmail.com>
2026-09-14 17:04:04 -0300

    feat: adicionar e consigurar ORM e adicionar nivel modelo e renomer datasource URL.

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import process, { loadEnvFile } from 'node:process';
+import { defineConfig } from 'prisma/config';
+
+loadEnvFile('./.env');
+
+export default defineConfig({
+	schema: './prisma/schema.prisma',
+	migrations: {
+		path: './prisma/migrations',
+		seed: 'tsx prisma/seed.ts'
+	},
+	datasource: {
+		url: process.env.DATABASE_URL
+	}
+});
