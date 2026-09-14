8cfc59832ce2a8b49422bd6155b1d480d5502493
gustavo martins <luizgustams@gmail.com>
2026-09-14 18:24:02 -0300

    feat: criar classe para validação de erros.

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,8 +1,21 @@
+import type { ValidationFieldError } from '../types.ts';
+
 export class NotFoundError extends Error {
 	statusCode: number;
 
 	constructor(message: string) {
 		super(message);
 		this.statusCode = 404;
+	}
+}
+
+export class ValidateError extends Error {
+	statusCode: number;
+	fields: ValidationFieldError[];
+
+	constructor(message: string, fields: ValidationFieldError[]) {
+		super(message);
+		this.statusCode = 404;
+		this.fields = fields;
 	}
 }
