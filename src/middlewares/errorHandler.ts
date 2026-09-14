c4d5620e46b0d3766f1e0e5decec96d4d8931a8e
gustavo martins <luizgustams@gmail.com>
2026-09-14 19:01:06 -0300

    feat: feat: incluir classe de validação de erros de campo na estrtura condicional.

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,27 @@
+import type { NextFunction, Request, Response } from 'express';
+import { NotFoundError, ValidationError } from '../errors/index.ts';
+import logger from '../lib/logger.ts';
+
+export default function errorHandler(
+	error: unknown,
+	_request: Request,
+	response: Response,
+	_next: NextFunction
+) {
+	if (error instanceof NotFoundError) {
+		response.status(error.statusCode).json({ message: error.message });
+		return;
+	}
+
+	if (error instanceof ValidationError) {
+		response.status(error.statusCode).json({
+			message: error.message,
+			fields: error.fields
+		});
+		return;
+	}
+
+	logger.error(error);
+
+	response.status(500).json({ message: 'Internal server error.' });
+}
