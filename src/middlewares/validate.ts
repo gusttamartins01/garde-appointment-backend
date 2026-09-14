6fff6b132463776802ede6cb51b4b4c52d26b981
gustavo martins <luizgustams@gmail.com>
2026-09-14 19:02:45 -0300

    feat: adiconar validação de erro para má requisições.

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,21 @@
+import type { NextFunction, Request, Response } from 'express';
+import type { ZodType } from 'zod';
+import { ValidationError } from '../errors/index.ts';
+
+export default function validate(schema: ZodType) {
+	return (request: Request, _response: Response, next: NextFunction) => {
+		const result = schema.safeParse(request.body);
+
+		if (!result.success) {
+			const fields = result.error.issues.map((issue) => ({
+				field: issue.path.join(''),
+				message: issue.message
+			}));
+
+			return next(new ValidationError('Invalid data', fields));
+		}
+
+		request.body = result.data;
+		next();
+	};
+}
